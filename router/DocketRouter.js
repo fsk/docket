3128eecf69a230fb0f56dc827e077b7127ff34ec
0xfsk <furkankulaksiz68@gmail.com>
2023-06-14 00:30:22 +0300

    delete endpointleri olusturuldu

--- router/DocketRouter.js
+++ router/DocketRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import docketController from "../controllers/DocketController.js";
-const { docketList, createDocket } = docketController;
+const { docketList, createDocket, deleteDocket } = docketController;
 
 const docketRouter = express.Router();
 
 docketRouter.get("/:id", docketList);
-docketRouter.post("/", createDocket)
+docketRouter.post("/", createDocket);
+docketRouter.delete("/:id", deleteDocket);
 
 export default docketRouter;
